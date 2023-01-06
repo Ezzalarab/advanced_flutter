a43083ea3bafff5c0f74e9a7915887d44fffb44d
Ezzalarab Al-Homaidi <ezz2019alarab@gmail.com>
2023-01-06 05:01:43 +0300

    add styles manager file & update presentation files names

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,91 @@
+import 'package:advanced_flutter/presentation/colors_manager.dart';
+import 'package:advanced_flutter/presentation/fonts_manager.dart';
+import 'package:flutter/cupertino.dart';
+
+TextStyle _getTextStyle({
+  required double fontSize,
+  required FontWeight fontWeight,
+  required Color fontColor,
+}) {
+  return TextStyle(
+    fontFamily: FontConstants.fontFamily,
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+    color: fontColor,
+  );
+}
+
+// Light Style
+
+TextStyle getLightStyle({
+  double fontSize = FontSize.s12,
+  FontWeight fontWeight = FontWeightManager.light,
+  Color fontColor = ColorManager.darkGrey,
+}) {
+  return TextStyle(
+    fontFamily: FontConstants.fontFamily,
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+    color: fontColor,
+  );
+}
+
+// Regular Style
+
+TextStyle getRegularStyle({
+  double fontSize = FontSize.s12,
+  FontWeight fontWeight = FontWeightManager.regular,
+  Color fontColor = ColorManager.darkGrey,
+}) {
+  return TextStyle(
+    fontFamily: FontConstants.fontFamily,
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+    color: fontColor,
+  );
+}
+
+// Medium Style
+
+TextStyle getMediuStyle({
+  double fontSize = FontSize.s12,
+  FontWeight fontWeight = FontWeightManager.medium,
+  Color fontColor = ColorManager.darkGrey,
+}) {
+  return TextStyle(
+    fontFamily: FontConstants.fontFamily,
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+    color: fontColor,
+  );
+}
+
+// SemiBold Style
+
+TextStyle getSemiBoldStyle({
+  double fontSize = FontSize.s12,
+  FontWeight fontWeight = FontWeightManager.semiBold,
+  Color fontColor = ColorManager.darkGrey,
+}) {
+  return TextStyle(
+    fontFamily: FontConstants.fontFamily,
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+    color: fontColor,
+  );
+}
+
+// Bold Style
+
+TextStyle getBoldStyle({
+  double fontSize = FontSize.s12,
+  FontWeight fontWeight = FontWeightManager.bold,
+  Color fontColor = ColorManager.darkGrey,
+}) {
+  return TextStyle(
+    fontFamily: FontConstants.fontFamily,
+    fontSize: fontSize,
+    fontWeight: fontWeight,
+    color: fontColor,
+  );
+}
