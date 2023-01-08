60aae8b186428d479b9f044aa4cecf9fc55ea92c
Ezzalarab Al-Homaidi <ezz2019alarab@gmail.com>
2023-01-08 04:51:49 +0300

    add route generator class

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,8 +1,51 @@
+import 'package:advanced_flutter/presentation/forgot_password/forgot_password_view.dart';
+import 'package:advanced_flutter/presentation/login/login_view.dart';
+import 'package:advanced_flutter/presentation/main/main_view.dart';
+import 'package:advanced_flutter/presentation/register/register_view.dart';
+import 'package:advanced_flutter/presentation/splash/splash_view.dart';
+import 'package:advanced_flutter/presentation/store_details/store_details_view.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 class Routes {
   static const String splashRoute = "/";
   static const String loginRoute = "/login";
   static const String registerRoute = "/register";
   static const String forgotPasswordRoute = "/forgot_password";
   static const String mainRoute = "/main";
-  static const String storeDetailsRoute = "/";
+  static const String storeDetailsRoute = "/store_details";
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => const SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case Routes.registerRoute:
+        return MaterialPageRoute(builder: (_) => const RegisterView());
+      case Routes.forgotPasswordRoute:
+        return MaterialPageRoute(builder: (_) => const ForgotPasswordView());
+      case Routes.mainRoute:
+        return MaterialPageRoute(builder: (_) => const MainView());
+      case Routes.storeDetailsRoute:
+        return MaterialPageRoute(builder: (_) => const StoreDetailsView());
+      default:
+        return unHandeledRoute();
+    }
+  }
+
+  static Route<dynamic> unHandeledRoute() {
+    return MaterialPageRoute(
+        builder: (_) => Scaffold(
+              appBar: AppBar(
+                title: const Text(
+                    "No Route Found"), // TODO move string to stringes manager
+              ),
+              body: const Center(
+                  child: Text(
+                      "No Route Found")), // TODO move string to stringes manager
+            ));
+  }
 }
