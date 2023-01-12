5907d5d3ebb01822df6e6eae8a1e45d5ad7f2e98
Ezzalarab Al-Homaidi <ezz2019alarab@gmail.com>
2023-01-12 04:20:38 +0300

    add svg pictures

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -1,5 +1,12 @@
-const String IMAGE_PATH = "assets/images";
+const String imagePath = "assets/images";
 
 class ImagesAssets {
-  static const String pytonLogo = "$IMAGE_PATH/python_logo.png";
+  static const String pytonLogo = "$imagePath/python_logo.png";
+  static const String artificialBrain =
+      "$imagePath/artificial-brain-computer-svgrepo-com.svg";
+  static const String artificialIntelligence =
+      "$imagePath/artificial-intelligence-svgrbar-chart-sorted-business-analytics-statistics-svgrepo-com.svg";
+  static const String chartStatistics =
+      "$imagePath/donut-chart-piece-business-analytics-statistics-svgrepo-com.svg";
+  static const String python = "$imagePath/icons8-python.svg";
 }
--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,3 +1,13 @@
 class AppSrtings {
   static const noRouteFound = "No Route Found";
+  static const onBoardingTitle1 = "SEE THE BEST COURSE #1";
+  static const onBoardingTitle2 = "SEE THE BEST COURSE #2";
+  static const onBoardingTitle3 = "SEE THE BEST COURSE #3";
+  static const onBoardingTitle4 = "SEE THE BEST COURSE #4";
+
+  static const onBoardingSubTitle1 =
+      "This app is an awesome flutter application using clean architecture #4";
+  static const onBoardingSubTitle2 = " #2";
+  static const onBoardingSubTitle3 = "SEE THE BEST COURSE #3";
+  static const onBoardingSubTitle4 = "SEE THE BEST COURSE #4";
 }
